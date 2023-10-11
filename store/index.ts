60cae7f53d45e561ff6e270425c40a5d51ab00dd
Faisal khan <fkk992638@gmail.com>
2023-10-11 16:23:15 +0530

    changed to productioj url

--- store/index.ts
+++ store/index.ts
@@ -2,6 +2,4 @@ export * from "./atoms";
 export * from "./helpers";
 export * from "./interfaces";
 
-// "https://sparkleworldstudio.com/api"   production url
-// "http://localhost:3000/api"
-export const baseUrl = "http://localhost:3000/api";
+export const baseUrl = "https://sparkleworldstudio.com/api";
