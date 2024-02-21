61c78bd32da8f182577f456b3ca369cd226ad16b
Faisal khan <fkk992638@gmail.com>
2024-02-22 00:34:57 +0530

    changed production url

--- store/index.ts
+++ store/index.ts
@@ -4,6 +4,6 @@ export * from "./interfaces";
 
 // BASE url for backend api
 // production
-export const baseUrl = "https://www.sparkleworldstudio.com/api";
+export const baseUrl = "https://sparkleworldstudio.com/api";
 // dev
 // export const baseUrl = "http://localhost:3000/api";
